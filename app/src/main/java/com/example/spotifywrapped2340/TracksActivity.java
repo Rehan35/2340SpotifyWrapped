a421956f93ef165dacad0ea147113296b283632e
Terrence Onodipe <terrenceonodipe@lawn-128-61-45-167.lawn.gatech.edu>
2024-04-20 21:13:45 -0400

    added spotify remote player, hardcoded to play one song on tracks activity

--- app/src/main/java/com/example/spotifywrapped2340/TracksActivity.java
+++ app/src/main/java/com/example/spotifywrapped2340/TracksActivity.java
@@ -1,17 +1,22 @@
 package com.example.spotifywrapped2340;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.spotifywrapped2340.SpotifyDataManagers.SpotifyManager;
 
-import jp.shts.android.storiesprogressview.StoriesProgressView;
+import jp.shts.android.storiesprogressview.StoriesProgressView;    import com.example.spotifywrapped2340.SpotifyDataManagers.SpotifyManager;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
 
 
 public class TracksActivity extends AppCompatActivity implements StoriesProgressView.StoriesListener{
@@ -23,6 +28,10 @@ public class TracksActivity extends AppCompatActivity implements StoriesProgress
     private int currentIndex = 0;
 
     private ImageView imageView;
+    private SpotifyAppRemote obj;
+
+
+
 
 
     @Override
@@ -34,6 +43,28 @@ public class TracksActivity extends AppCompatActivity implements StoriesProgress
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wrapped);
+
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder("3f2eac4dbbb0498194d8b5d955949c1a")
+                        .showAuthView(true)
+                        .build();
+        SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener() {
+            @Override
+            public void onConnected(@NonNull SpotifyAppRemote spotifyAppRemote) {
+                obj = spotifyAppRemote;
+                Log.d("MainActivity", "Connected to Spotify!");
+
+                // Now you can use mSpotifyAppRemote to control playback, etc.
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                Log.e("MainActivity", "Failed to connect to Spotify", throwable);
+                // Handle connection failure
+            }
+        });
+        obj.getPlayerApi().play("spotify:track:4uLU6hMCjMI75M1A2tKUQC");
+
         topLabel = (TextView) findViewById(R.id.topLabel);
         topLabel.setText("Top Tracks!");
 
